4a984545e25c02b044628f0a45f6c282b17390c7
Azzeras <aeneas62@hotmail.fr>
2017-06-14 02:04:54 +0200

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -227,16 +227,4 @@ class Candy(pg.sprite.Sprite):
             if indice != None :
                 del self.game.candy_List[indice]
 
-class Wall(pg.sprite.Sprite):
-    def __init__(self, game, x, y):
-        self.groups = game.all_sprites, game.walls
-        pg.sprite.Sprite.__init__(self,self.groups)
 
-        self.game = game
-        self.image = pg.Surface((TAILLE_SERPENT, TAILLE_SERPENT))
-        self.image.fill(RED)
-        self.rect=self.image.get_rect()
-        self.x=x
-        self.y=y
-        self.rect.x=x*TAILLE_SERPENT
-        self.rect.y=y*TAILLE_SERPENT
